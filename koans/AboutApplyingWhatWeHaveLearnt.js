c3685acd94a474513b98ee5e0beddae467f66d5e
tetondan <dfrehner1@gmail.com>
2015-10-22 09:26:29 -1000

    Completed Javascript Koans

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -30,15 +30,16 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
       var productsICanEat = [];
+      var productsIcanEat1 = _(products).filter(function (a) {return a.containsNuts === false});
+          productsICanEat.push(_(productsIcanEat1).any(function(x){x.ingredients !== "mushrooms"; return x;});                  
+                     
 
-      /* solve using filter() & all() / any() */
-
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+      expect(productsICanEat.length).toBe(1);
   });
 
   /*********************************************************************************/
@@ -52,13 +53,13 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
     
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
-
-    expect(233168).toBe(FILL_ME_IN);
+    var sum = _.range(0,1001).reduce(function(a,b){if(b % 3 === 0 || b % 5 === 0){return a + b;}}, 0);    /* try chaining range() and reduce() */
+    
+    expect(sum).toBe(233168);
   });
 
   /*********************************************************************************/
